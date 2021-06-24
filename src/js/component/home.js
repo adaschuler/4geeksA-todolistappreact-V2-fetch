a8e794fcc602aaad37ec91c09a86a73a604ac2e7
adaschuler <adaschuler@gmail.com>
2021-06-24 20:53:57 +0000

    ejercicio

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 export function Home() {
+	// 01 - Crear un array
+	const [todos, setTodos] = useState(["tarea 1", "tarea 2", "tarea 3"]);
+	/// 02 guardar texto que digito el usuario
+	const [task, setTask] = useState("");
+	function todosEliminar(index) {
+		if (index > -1) {
+			const filterList = todos.filter(item => item !== todos[index]);
+			setTodos(filterList);
+		}
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		// 03 Hacer el formulario
+		<div className="text-center mt-5 container">
+			<h1>TODO LIST WITH REACT</h1>
+			<form
+				onSubmit={evento => {
+					evento.preventDefault();
+					if (task.length > 0) setTodos([...todos, task]);
+					setTask("");
+				}}>
+				<input
+					className="form-control form-control-lg"
+					placeholder="Ingrese su tarea"
+					onChange={evento => setTask(evento.target.value)}
+					value={task}></input>
+			</form>
+			{/* 04 mostrar el contenido del arreglo */}
+			<ul>
+				{todos.map((item, index) => {
+					return (
+						<li key={index}>
+							<span>{item}</span>
+							<button
+								className="btn btn-light float-right"
+								onClick={() => {
+									todosEliminar(index);
+								}}>
+								<i className="far fa-star"></i>
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+			<div className="pendientes">{todos.length} pendiente/s</div>
 		</div>
 	);
 }
